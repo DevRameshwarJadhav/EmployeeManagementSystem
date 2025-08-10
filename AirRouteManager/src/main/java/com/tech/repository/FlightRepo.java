f9d0aa4e44a1a6d0920c4284ca939d27f3504e58
DevRameshwarJadhav <jrameshwar572@gmail.com>
2025-08-10 13:14:39 +0530

    Delete

--- AirRouteManager/src/main/java/com/tech/repository/FlightRepo.java
+++
@@ -1,9 +0,0 @@
-package com.tech.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.tech.entity.Flight;
-
-public interface FlightRepo extends CrudRepository<Flight, String> {
-
-}
